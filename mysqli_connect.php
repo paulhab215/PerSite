f3babf03d7d19bbc5f08651ca6516543650413d6
Paul Habjanetz <paulhab39@gmail.com>
2017-08-26 10:58:54 -0500

    Format

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,8 +1,4 @@
-<?php # Script 9.2 - mysqli_connect.php
-
-// This file contains the database access information. 
-// This file also establishes a connection to MySQL, 
-// selects the database, and sets the encoding.
+<?php # Database Access - establishes connection wile setting encoding
 
 // Set the database access information as constants:
 DEFINE ('DB_USER', '');
@@ -10,8 +6,7 @@ DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'sitename');
 
-// Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
 
-// Set the encoding...
+// Set encoding.
 mysqli_set_charset($dbc, 'utf8');
